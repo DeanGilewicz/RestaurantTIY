e88f37bc0e5b09aa2e7999e8b0c2106edc6bfaac
Joanna Edgell <joedgell2013@gmail.com>
2014-10-15 10:51:14 -0400

    base template language js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,16 @@
 var api_menu = 'http://restaurantapi.apiary-mock.com/menu';
 var apt_flickr ;
 var api_special = 'http://restaurantapi.apiary-mock.com/menu/special';
-var api_news = 'http://restaurantapi.apiary-mock.com/news/latest',
-    template_menu,
-    template_flickr,
-    template_special,
-    template_news,
+var api_news = 'http://restaurantapi.apiary-mock.com/news/latest';
+
+var template_menu= $('').html();
+var render_menu = _.template(template_menu);
+
+var template_flickr= $('').html();
+var render_flickr = _.template(template_flickr);
+
+var template_special= $('').html();
+var render_special = _.template(template_special);
+
+var template_news= $('').html();
+var render_news = _.template(template_news);
