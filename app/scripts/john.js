479a6f0591333dc498937cf2e0ade05fece47c89
John Crocker <johnniecrocker@yahoo.com>
2014-10-15 12:59:20 -0400

    api call to news working

--- app/scripts/john.js
+++ app/scripts/john.js
@@ -9,4 +9,15 @@ var render_flickr ;// = _.template(template_flickr);
 var template_news ;//= $('').html();
 var render_news ; //= _.template(template_news);
 
-var
+var news_title,
+    news_text;
+
+//This calls the news API and retrieves the content for the section.
+
+    $.getJSON(api_news).done( function(news_data){
+
+          news_title = news_data.title;
+
+          news_text = news_data.post;
+          //console.log(news_text);
+        });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,14 @@ var apt_flickr ;
 var api_special = 'http://restaurantapi.apiary-mock.com/menu/special';
 var api_news = 'http://restaurantapi.apiary-mock.com/news/latest';
 
-var template_menu= $('').html();
-var render_menu = _.template(template_menu);
+//var template_menu= $('').html();
+//var render_menu = _.template(template_menu);
 
-var template_flickr= $('').html();
-var render_flickr = _.template(template_flickr);
+//var template_flickr= $('').html();
+//var render_flickr = _.template(template_flickr);
 
-var template_special= $('').html();
-var render_special = _.template(template_special);
+//var template_special= $('').html();
+//var render_special = _.template(template_special);
 
-var template_news= $('').html();
-var render_news = _.template(template_news);
+//var template_news= $('').html();
+//var render_news = _.template(template_news);
