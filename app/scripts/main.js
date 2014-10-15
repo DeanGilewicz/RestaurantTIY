8fce5a436a03bb1808326a929a824560da1a160e
Joanna Edgell <joedgell2013@gmail.com>
2014-10-15 15:59:48 -0400

    initial styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,15 @@
-//API variables
+//API variables for menu and specials
 var api_menu = 'http://restaurantapi.apiary-mock.com/menu';
-var apt_flickr ;
 var api_special = 'http://restaurantapi.apiary-mock.com/menu/special';
-var api_news = 'http://restaurantapi.apiary-mock.com/news/latest';
+
 
 //Main menu jquery for template
 var template_menu= $('#template_menu').html();
 var render_menu = _.template(template_menu);
 
 $.getJSON(api_menu).done( function(menu_data){
-
     _.each(menu_data.appetizers, function (apps){
-
-    $('.menu_apps').append(render_menu(apps));
+           $('.menu_apps').append(render_menu(apps));
     })
 
     _.each(menu_data.entrees, function (entree){
@@ -27,16 +24,9 @@ $.getJSON(api_menu).done( function(menu_data){
 
 });
 
-
-/*var template_flickr= $('').html();
-var render_flickr = _.template(template_flickr);
-
+/*
 var template_special= $('').html();
-var render_special = _.template(template_special);
-
-var template_news= $('').html();
-var render_news = _.template(template_news);*/
-
+var render_special = _.template(template_special);*/
 
 // Tab Functionality
 $('#story').click(function(){
