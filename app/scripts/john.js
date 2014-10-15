8c3db93942a81f3ceb17c395bb3660a00c744c0b
John Crocker <johnniecrocker@yahoo.com>
2014-10-15 12:50:32 -0400

    added john.js to index and initial var to john.js

---
+++ app/scripts/john.js
@@ -0,0 +1,12 @@
+
+
+var apt_flickr ;  // Flickr API
+var api_news = 'http://restaurantapi.apiary-mock.com/news/latest'; //news api
+
+var template_flickr ;//= $('').html();
+var render_flickr ;// = _.template(template_flickr);
+
+var template_news ;//= $('').html();
+var render_news ; //= _.template(template_news);
+
+var
