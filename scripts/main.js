bba01626d459c80b04372589ac96f307b853d9b4
Joanna Edgell <joedgell2013@gmail.com>
2014-10-15 10:45:57 -0400

    some api added

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var api_menu = 'http://restaurantapi.apiary-mock.com/menu';
+var apt_flickr ;
+var api_special = 'http://restaurantapi.apiary-mock.com/menu/special';
+var api_news = 'http://restaurantapi.apiary-mock.com/news/latest',
+    template_menu,
+    template_flickr,
+    template_special,
+    template_news,
