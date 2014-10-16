a703c33b8b723822493001e2467969242a05a82b
Joanna Edgell <joedgell2013@gmail.com>
2014-10-16 11:24:37 -0400

    holy specials 2x

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,40 @@
 //API variables for menu and specials
+var special_item;
+var special_price;
+var special_description;
+
+var template_menu=$('#template_menu').html();
+var render_menu=_.template(template_menu);
+
+var template_special=$('#template_special').html();
+var render_special=_.template(template_special);
+
 var api_menu='http://restaurantapi.apiary-mock.com/menu';
 var api_special= 'http://restaurantapi.apiary-mock.com/menu/special';
 
 
-//Main menu jquery for template
-var template_menu=$('#template_menu').html();
-var render_menu=_.template(template_menu);
+//Main menu & specials jquery for template
+var specialID;
+$.getJSON(api_special).done (function(special_data){
+  specialID = special_data.menu_item_id;
+});
 
 $.getJSON(api_menu).done (function(menu_data){
+
+    //using menu api for specials
+      _.each(menu_data, function (looking){
+
+       _.each(looking, function (all){
+
+        if (all.id === specialID) {
+          special_item = (all.item);
+          special_price = (all.price);
+          special_description = (all.description);
+      $('.special_info').append(render_special(special_item));
+        }
+      })
+  })
+
 
     _.each(menu_data.appetizers, function (apps){
            $('.menu_apps').append(render_menu(apps));
@@ -21,38 +48,9 @@ $.getJSON(api_menu).done (function(menu_data){
     _.each(menu_data.sides, function (side){
 
     $('.menu_sides').append(render_menu(side));
-    })
-
+  })
 });
 
-
-// Specials section
-var special_item;
-var special_price;
-var special_description;
-var template_special=$('#template_special').html();
-var render_special=_.template(template_special);
-
-$.getJSON(api_menu).done (function(menu_data2){
-
-  _.each(menu_data2, function (looking){
-
-  _.each(looking, function (all){
-
-
-  $.getJSON(api_special).done (function(special_data){
-
-    if (all.id == special_data.menu_item_id) {
-      special_item = (all.item);
-      special_price = (all.price);
-      special_description = (all.description);
-
- $('.special_info').append(render_special(special_item));
-}
-})
-})
-})
-});
 
 // Tab Functionality
 $('#story').click(function(){
