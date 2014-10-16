c170597f81368e33845f2cd8e319717180c052d9
Joanna Edgell <joedgell2013@gmail.com>
2014-10-15 20:42:11 -0400

    attempt at filter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
 //API variables for menu and specials
-var api_menu = 'http://restaurantapi.apiary-mock.com/menu';
-var api_special = 'http://restaurantapi.apiary-mock.com/menu/special';
-
+var api_menu='http://restaurantapi.apiary-mock.com/menu';
+var api_special= 'http://restaurantapi.apiary-mock.com/menu/special';
 
 
 //Main menu jquery for template
-var template_menu= $('#template_menu').html();
-var render_menu = _.template(template_menu);
+var template_menu=$('#template_menu').html();
+var render_menu=_.template(template_menu);
 
-$.getJSON(api_menu).done( function(menu_data){
+$.getJSON(api_menu).done (function(menu_data){
+
     _.each(menu_data.appetizers, function (apps){
            $('.menu_apps').append(render_menu(apps));
     })
@@ -26,9 +26,28 @@ $.getJSON(api_menu).done( function(menu_data){
 
 
 // Specials section
-//var template_special= $('').html();
-//var render_special = _.template(template_special);*/
+var template_special=$('#template_special').html();
+var render_special=_.template(template_special);
 
+$.getJSON(api_menu).done (function(menu_data2){
+
+  _.each(menu_data2, function (looking){
+
+  _.each(looking, function (all){
+
+  $.getJSON(api_special).done (function(special_data){
+
+    var matches = _.findWhere(special_data == all);
+
+
+//var match = _.findWhere (all.id == special_data.menu_item_id);
+
+
+  //$('.special').append(render_special());
+})
+})
+})
+});
 
 // Tab Functionality
 $('#story').click(function(){
