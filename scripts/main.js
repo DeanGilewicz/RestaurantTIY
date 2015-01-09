0b4eced9b8df2d628e3d55ce8580f58c13fc1b0c
Dean Gilewicz <gilewiczd@gmail.com>
2015-01-09 14:35:47 -0500

    ready to deploy to my github

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-var api_flickr = 'https://api.flickr.com/services/rest/?&method=flickr.photos.search&tags=food&tag_mode=all&license=7&api_key=' + 'e1126579a99d5f23c6802a189f95b9dd' + '&per_page=6&page=1&format=json&nojsoncallback=1';
+var api_flickr = 'https://api.flickr.com/services/rest/?&method=flickr.photos.search&tags=food&tag_mode=all&license=1&api_key=' + 'e1126579a99d5f23c6802a189f95b9dd' + '&per_page=6&page=1&format=json&nojsoncallback=1';
 
 // Flickr API uses method photos.search, uses license key = 7-No known copyright restrictions.
 
@@ -60,6 +60,8 @@ var secret;
             food_photo = 'https://farm' + farm_id +'.staticflickr.com/' + server_id + '/' + id + '_' + secret + '_m.jpg';
 
           //  console.log(food_photo);
+
+            console.log(photo.owner);
 
 
             $('.flickr').append(render_flickr(photo));
